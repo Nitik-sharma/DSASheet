1e419f09245ee848ad95d7fcdb7a4d3fa83a5818
Nitik sharma <sharmanitik36@gmail.com>
2024-03-02 15:51:15 +0530

    Selection sort learend

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 // bubble sort
 
-const bubbleSort = (arr) => {
+// const bubbleSort = (arr) => {
+//   const n = arr.length;
+//   for (let i = 0; i < n; i++) {
+//     for (j = 0; j < n - i - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//       }
+//     }
+//   }
+//   return arr;
+// };
+
+// const ans = bubbleSort([89, 90, 87, 68, 100, 57]);
+// console.log("ans", ans);
+
+// Selection short
+
+const selectionShort = (arr) => {
   const n = arr.length;
-  for (let i = 0; i < n; i++) {
-    for (j = 0; j < n - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+  for (let i = 0; i < n - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
       }
+    }
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
     }
   }
   return arr;
 };
 
-const ans = bubbleSort([89, 90, 87, 68, 100, 57]);
-console.log("ans", ans);
+console.log(selectionShort([90, 87, 199, 40, 50]));
